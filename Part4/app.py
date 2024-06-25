c641fa576ed60367aefdc7504f20b38f8dfb886e
Alok <alokrajpd@gmail.com>
2024-06-25 11:55:39 +0530

    Part 4 added

--- Part4/app.py
+++ Part4/app.py
@@ -1,25 +1,43 @@
-from flask import Flask , render_template , url_for , redirect
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    url_for,
+    flash
+)
+from forms import SignupForm, LoginForm
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "this_is_a_secret_key"
 
-from forms import SignupForm , LoginForm
-
-app.config["SECRET_KEY"] ="this_is_a_secret_key"
 @app.route("/")
-
 @app.route("/home")
 def home():
-    return render_template("home.html" , title="Home")
-@app.route("/signup",methods=["GET","POST"])
+    return render_template("home.html", title="Home")
+
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
-        print("Hi")
+        flash(f"Successfully Registered {form.username.data}!")
         return redirect(url_for("home"))
-    return render_template("signup.html" , title="Signup" , form=form)
-@app.route("/login")
+    return  render_template("signup.html", title="Sign Up", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    form =LoginForm()
-    return render_template("login.html" ,title="Login",form=form)
+    form = LoginForm()
+    email = form.email.data
+    pw = form.password.data
+    if form.validate_on_submit():
+        if email == "a@b.com" and pw == "12345":
+            flash("Logged in Successfully!")
+            return redirect(url_for("home"))
+        else:
+            flash("Incorrect email or password")
+    return  render_template("login.html", title="Login", form=form)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- Part4/forms.py
+++ Part4/forms.py
@@ -1,39 +1,48 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField,
-                     SelectField,
-                    PasswordField,
-                     SubmitField,
-                    BooleanField,
-                    DateField
-                     )
-
+from wtforms import (
+    StringField,
+    SelectField,
+    DateField,
+    PasswordField,
+    SubmitField,
+    BooleanField
+)
 from wtforms.validators import (
     DataRequired,
     Length,
     Email,
     Optional,
-    equal_to
+    EqualTo
 )
+
+
 class SignupForm(FlaskForm):
     username = StringField(
-        "username",
-        validators=[DataRequired(),Length(2,30)]
+        "Username",
+        validators=[DataRequired(), Length(2, 30)]
     )
     email = StringField(
         "Email",
-        validators=[DataRequired() , Email()]
+        validators=[DataRequired(), Email()]
     )
-    gender = SelectField("Gender",
-                         choices = ["Male" , "Female" , "Other"],
-                         validators=[Optional()])
-    dob = DateField("DOB",
-                    validators=[Optional()])
-    password = PasswordField("Password" ,
-                             validators=[DataRequired() ,Length(5,25)])
-    confirm_password = PasswordField("Confirm Password",
-                             validators=[DataRequired(), Length(5, 25),equal_to("password")])
-
-    submit = SubmitField("Sign up")
+    gender = SelectField(
+        "Gender",
+        choices=["Male", "Female", "Other"],
+        validators=[Optional()]
+    )
+    dob = DateField(
+        "Date of Birth",
+        validators=[Optional()]
+    )
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired(), Length(5, 25)]
+    )
+    confirm_password = PasswordField(
+        "Confirm Password",
+        validators=[DataRequired(), Length(5, 25), EqualTo("password")]
+    )
+    submit = SubmitField("Sign Up")
 
 
 class LoginForm(FlaskForm):
@@ -41,8 +50,9 @@ class LoginForm(FlaskForm):
         "Email",
         validators=[DataRequired(), Email()]
     )
-    password = PasswordField("Password",
-                             validators=[DataRequired(), Length(5, 25)])
-    remember_me=BooleanField("Remember Me")
-
-    submit = SubmitField("Sign up")
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired(), Length(5, 25)]
+    )
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Login")
